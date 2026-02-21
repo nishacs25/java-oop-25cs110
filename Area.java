9492035ae36153002a54a9639ef4914e0be0ed8b
nishacs25 <nisha.k19285@gmail.com>
2026-02-22 01:55:35 +0500

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,12 @@
+public class Area {
+    public static void main(String[] args) {
+
+        int w = 12;
+        int h = 6;
+        int area = w * h;
+
+        System.out.println("Width: " + w);
+        System.out.println("Height: " + h);
+        System.out.println("Area: " + area);
+    }
+}
