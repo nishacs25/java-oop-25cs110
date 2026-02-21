8fe5e872d5d961098c23492e9f115cc8898859d3
nishacs25 <nisha.k19285@gmail.com>
2026-02-22 01:41:21 +0500

    Create Friend.java

---
+++ Friend.java
@@ -0,0 +1,20 @@
+public class Friend {
+    public static void main(String[] args) {
+
+        String s1 = "Yusra";
+        String s2 = "Rida";
+        
+        String mood1 = " (X_X)"; 
+        String mood2 = " (~_~)";
+        
+        int studyHours = 0;
+        int chillTime = 100;
+
+        System.out.println("Student Name: " + s1 + mood1);
+        System.out.println("Student Name: " + s2 + mood2);
+        
+        System.out.println("Total Study: " + studyHours);
+        System.out.println("Total Chill: " + chillTime);
+        System.out.println("Status: System Error 404");
+    }
+}
