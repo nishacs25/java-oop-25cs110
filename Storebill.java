09f0e8d9af6b13429e3ea517cccae2f87cb56c8c
nishacs25 <nisha.k19285@gmail.com>
2026-02-22 01:28:36 +0500

    Create Storebill.java

---
+++ Storebill.java
@@ -0,0 +1,18 @@
+class Storebill {
+    public static void main(String[] args) {
+
+        String store_name = "outfitters";
+        String product_name = "shoes";
+        int price = 4500;
+        int quantity = 2;
+
+        double total_price = price * quantity;
+
+        System.out.println("store name:" + store_name);
+        System.out.println("product name:" + product_name);
+        System.out.println("price:" + price);
+        System.out.println("quantity" + quantity);
+        System.out.println("the total price is:" + total_price);
+    }
+
+}
