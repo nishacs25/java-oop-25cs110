ec6ea41cd412840f257db8f3a376138facf255a8
nishacs25 <nisha.k19285@gmail.com>
2026-02-22 01:47:28 +0500

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Division: " + div);
+    }
+}
