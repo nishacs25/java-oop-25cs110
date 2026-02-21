95108ee454c99c54933f63a936c2547699a028ce
nishacs25 <nisha.k19285@gmail.com>
2026-02-22 01:19:06 +0500

    Create Rollnos.java

---
+++ Rollnos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Rollnos {
+  
+	public static void main(String[] args) {    
+	  String university= "MEHRAN UNIVERSITY OF ENGINEERING AND TECHNOLOGY";
+      String department= "DEPARTMENT OF COMPUTER SYSTEM ENGINEERING";
+      
+      System.out.println( "University Name: " + university );
+      System.out.println( "Department Name : " + department);
+      
+      for(int i=1 ; i<=30 ; i++ )
+    	  System.out.println("ROLL NO : 25CS0" + i );
+      
+	}
+}
